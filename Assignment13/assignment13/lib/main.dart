8ef3be6da7d7e28fafc9779a0d6ae0aed2bab96a
Nikeeta_Patil <nikeetapatil212>
2024-05-04 19:58:46 +0530

    assignments

---
+++ Assignment13/assignment13/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "MyApp",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Three Containers"),
+        ),
+        body: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceAround, //by deflut MainAxisAlignmnet.start
+          //by defalut MainAXisalignmnet.start
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            Container(height: 100, width: 100, color: Colors.red),
+            Container(height: 100, width: 100, color: Colors.green),
+            Container(height: 100, width: 100, color: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
